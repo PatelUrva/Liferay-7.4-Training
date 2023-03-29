eecbc64d7e14f36dca2a8c6ab2a404b8d5d382a7
Urva Patel <122914492+PatelUrva@users.noreply.github.com>
2023-03-29 12:35:05 +0530

    Add files via upload

--- color-preferences/src/main/java/com/aixtor/portal/color/preferences/constants/ColorPreferencesPortletKeys.java
+++ color-preferences/src/main/java/com/aixtor/portal/color/preferences/constants/ColorPreferencesPortletKeys.java
@@ -1,1 +1,11 @@
+package com.aixtor.portal.color.preferences.constants;
 
+/**
+ * @author Urva Patel
+ */
+public class ColorPreferencesPortletKeys {
+
+	public static final String COLORPREFERENCES =
+		"com_aixtor_portal_color_preferences_ColorPreferencesPortlet";
+
+}
