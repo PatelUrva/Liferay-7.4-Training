d091d8f1e0772b249382bfb1c0c6742b724778ae
Urva Patel <122914492+PatelUrva@users.noreply.github.com>
2023-03-29 12:32:29 +0530

    Add files via upload

--- color-preferences/src/main/java/com/aixtor/portal/color/preference/ColorConfiguration.java
+++ color-preferences/src/main/java/com/aixtor/portal/color/preference/ColorConfiguration.java
@@ -1,1 +1,19 @@
+package com.aixtor.portal.color.preference;
 
+/**
+ * @author Urva Patel
+ */
+import aQute.bnd.annotation.metatype.Meta;
+
+@Meta.OCD(id = "com.aixtor.portal.color.preference.ColorConfiguration")
+public interface ColorConfiguration {
+	
+    @Meta.AD(
+        name = "color", 
+        optionLabels = { "%White", "%Red", "%Yellow" }, 
+        optionValues = { "white", "red", "yellow" }, 
+        required = false
+    )
+ 
+    public String color();
+}
