c7f907a92083d05e471e04ef87b5ad0550792cc4
Urva Patel <122914492+PatelUrva@users.noreply.github.com>
2023-03-29 12:14:26 +0530

    Add files via upload

--- employee-training-web/src/main/java/com/aixtor/training/employee/constants/EmployeeConstants.java
+++ employee-training-web/src/main/java/com/aixtor/training/employee/constants/EmployeeConstants.java
@@ -1,1 +1,66 @@
+package com.aixtor.training.employee.constants;
 
+/**
+ * @author Urva Patel
+ */
+public class EmployeeConstants {
+
+	public static final String BRANCH_NAME = "branchName";
+	public static final String COUNTRY = "country";
+	public static final String STATE = "state";
+	public static final String CITY = "city";
+	public static final String ADDRESS1 = "address1";
+	public static final String ADDRESS2 = "address1";
+	public static final String PINCODE = "pincode";
+	
+	public static final String BRANCH_ID = "branchId";
+	public static final String COUNTRY_ID = "countryId";
+	public static final String STATE_ID = "stateId";
+	public static final String CITY_ID = "cityId";
+	public static final String DEPARTMENT_ID = "departmentId";
+	public static final String DESIGNATION_ID = "designationId";
+	public static final String EMPLOYEE_ID = "employeeId";
+	
+	public static final String SEARCH_TEXT = "searchText";
+	
+	public static final String CITY_NAME = "cityName";
+	
+	public static final String STATE_NAME = "stateName";
+	
+	public static final String SELECTED_BRANCH = "selectedBranch";
+	
+	public static final String REDIRECT_URL = "redirectURL";
+	
+	public static final String BRANCH_LIST = "branchList";
+	public static final String COUNTRY_LIST = "countryList";
+	public static final String STATE_LIST = "stateList";
+	public static final String CITY_LIST = "cityList";
+	public static final String DESIGNATION_LIST = "designationList";
+	public static final String DEPARTMENT_LIST = "departmentList";
+	public static final String EMPLOYEE_LIST = "employeeList";
+	
+	public static final String DEPARTMENT_NAME = "departmentName";
+	public static final String DEPARTMENT_HEAD = "departmentHead";
+	
+	public static final String DESIGNATION_NAME = "designationName";
+	
+	public static final String FROM_DATE = "fromDate";
+	public static final String TO_DATE = "toDate";
+	
+	public static final String EMPLOYEE_NAME = "employeeName";
+	public static final String EMPLOYEE_EMAIL = "employeeEmail";
+	public static final String EMPLOYEE_MOBILE = "employeeMobile";
+	public static final String EMPLOYEE_DEPARTMENT = "employeeDepartment";
+	public static final String EMPLOYEE_DESIGNATION = "employeeDesignation";
+	public static final String EMPLOYEE_BRANCH = "employeeBranch";
+	
+	public static final String DATA = "data";
+	public static final String STATUS = "status";
+	public static final String SUCCESS = "success";
+	public static final String ERROR = "error";
+	public static final String MESSAGE = "message";
+	
+	public static final String MAIL_SUBJECT = "Liferay Mail";
+	public static final String SENDER_MAIL = "urvapatel1507@gmail.com";
+	public static final String RECEIVER_MAIL = "emailId";
+}
